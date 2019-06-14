2d41e35c63513652108acbdfa8a2db10bc972868
Jeremy <43865066+jerameee@users.noreply.github.com>
2019-06-14 16:17:57 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.chatapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
